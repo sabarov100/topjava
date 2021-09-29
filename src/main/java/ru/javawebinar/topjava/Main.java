7f5d9df8cb81dea7eba13e2f72d28df4c32f96ca
Evgeny Sabarov <e.sabarov@gmail.com>
2021-09-29 15:02:31 +0300

    chek commit

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava;
 /**
  * @see <a href="http://topjava.herokuapp.com">Demo application</a>
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
+ * commit
  */
 public class Main {
     public static void main(String[] args) {
